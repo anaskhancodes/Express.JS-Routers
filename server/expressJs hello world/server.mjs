f65768cc04dc23144bc98fa7d67cce898ae59cf0
M.Anas Khan <a4anas406@gmail.com>
2023-07-20 20:34:47 +0500

    ExpressJs Router

--- server/expressJs hello world/server.mjs
+++ server/expressJs hello world/server.mjs
@@ -6,16 +6,17 @@ import commentRouter from "./Routes/comments.mjs";
 import feedRouter from "./Routes/feed.mjs";
 import postRouter from "./Routes/post.mjs";
 import cors from "cors";
+import router from './Routes/auth.mjs';
 
 
 const app = express();
-app.use(express.json())
+// app.use(express.json())
 app.use(cors())
 
 app.use(authRouter);
 
 
-app.use((req,res,next) =>{
+router.use((req,res,next) =>{
   if(token === "Valid"){
     next();
   }else{
