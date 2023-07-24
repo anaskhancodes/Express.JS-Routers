27a10e6bd9a200a6b814819d9c1c3162e9aed6cc
M.Anas Khan <a4anas406@gmail.com>
2023-07-24 19:46:43 +0500

    ExpressJs Router

--- server/expressJs hello world/Routes/feed.mjs
+++ server/expressJs hello world/Routes/feed.mjs
@@ -5,7 +5,7 @@ let router = express.Router()
 
 
 //Get    /api /v1/feed/:userId 
-router.get('/api/v1/feed/:userId', (req, res) => {
+router.get('https://peaceful-chaja-0720c9.netlify.app/api/v1/feed/:userId', (req, res) => {
     console.log("This is FEED => ", Date())
     res.send("This is FEED => " + Date())
   })
